14b5320cd321b7ab3fe3597898cd471275655de9
Mike Barberry <mikebarberry@protonmail.com>
2020-11-23 07:39:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,22 +4,9 @@ class Dog
     @breed = breed
   end
 
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
-end
+  attr_accessor :name
+  attr_accessor :breed
+end 
 
 
 fido = Dog.new("Fido", "beagle")
